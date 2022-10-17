751a7d40298ef754141dbdf23addb16fae4aa563
npardhe <110229384+npardhe@users.noreply.github.com>
2022-10-17 08:59:07 +0530

    allow host

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-ojguh@+=vv%=masa7(8bl2ur7ygb^(n0xi#!8-(8d10*$0--gf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+
 
 
 # Application definition
